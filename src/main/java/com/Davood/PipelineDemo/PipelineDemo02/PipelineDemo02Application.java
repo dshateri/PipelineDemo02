ce87f884110314dc2333fab8060d3131bd31ca31
student <dshateri@gmail.com>
2021-06-19 16:56:29 +0000

    first commit

---
+++ src/main/java/com/Davood/PipelineDemo/PipelineDemo02/PipelineDemo02Application.java
@@ -0,0 +1,13 @@
+package com.Davood.PipelineDemo.PipelineDemo02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineDemo02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineDemo02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Davood/PipelineDemo/PipelineDemo02/PipelineDemo02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Davood.PipelineDemo.PipelineDemo02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineDemo02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
